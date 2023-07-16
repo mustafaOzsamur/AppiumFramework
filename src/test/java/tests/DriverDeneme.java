8f0e9cd3690bf201dcbf92dda3fdc4a7b529e574
MUSTAFA ÖZSAMUR <mustafaozsamur22@gmail.com>
2023-07-16 14:23:22 +0200

    16.07/V1

---
+++ src/test/java/tests/DriverDeneme.java
@@ -0,0 +1,14 @@
+package tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.testng.annotations.Test;
+import utils.Driver;
+
+public class DriverDeneme {
+    AndroidDriver<AndroidElement> driver= Driver.getAndroidDriver();
+    @Test
+    public void test01(){
+
+    }
+}
